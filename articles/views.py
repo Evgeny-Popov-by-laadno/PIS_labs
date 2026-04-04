e685683a7d9585a949cb0da830379d19ce07a616
Evgeny Popov <Evgeniy.popov2005@yandex.ru>
2026-04-04 19:10:18 +0300

    Лабораторная работа №4

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,15 @@
-from .models import Article
+# articles/views.py
 from django.shortcuts import render
+from django.http import Http404
+from .models import Article
 
 def archive(request):
-  return render(request, 'archive.html', {"posts": Article.objects.all()})
+    posts = Article.objects.all().order_by('-created_date')
+    return render(request, 'archive.html', {'posts': posts})
+
+def get_article(request, article_id):
+    try:
+        post = Article.objects.get(id=article_id)
+        return render(request, 'article.html', {'post': post})
+    except Article.DoesNotExist:
+        raise Http404 
