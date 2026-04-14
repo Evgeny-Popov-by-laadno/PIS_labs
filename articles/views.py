6c857aef245c595523b94fcbcc22147a1dcaad41
Evgeny Popov <Evgeniy.popov2005@yandex.ru>
2026-04-14 19:48:43 +0300

    Лабораторная работа №5

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 # articles/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Article
 
@@ -13,3 +13,31 @@ def get_article(request, article_id):
         return render(request, 'article.html', {'post': post})
     except Article.DoesNotExist:
         raise Http404 
+
+def create_post(request):
+    if not request.user.is_authenticated:  
+        raise Http404
+    
+    if request.method == "POST":
+        form = {
+            'text': request.POST["text"],
+            'title': request.POST["title"]
+        }
+        
+        if Article.objects.filter(title=form["title"]).exists():
+            form['errors'] = u"Статья с таким названием уже существует"
+            return render(request, 'create_post.html', {'form': form})
+        
+        if form["text"] and form["title"]:
+            article = Article.objects.create(
+                text=form["text"],
+                title=form["title"],
+                author=request.user
+            )
+            return redirect('get_article', article_id=article.id)
+        else:
+            form['errors'] = u"Не все поля заполнены"
+            return render(request, 'create_post.html', {'form': form})
+    else:
+
+        return render(request, 'create_post.html', {})
